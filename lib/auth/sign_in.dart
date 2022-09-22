4ac4d16fed5867ae12a6637a1e8256c63b4e7e8b
mahfuzur <mdmahfuazur7788@gamil.com>
2022-09-23 02:46:59 +0600

    singin file all most work now

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
 
 
 
@@ -8,39 +10,68 @@ class SignIn extends StatelessWidget {
     return Scaffold(
 
       body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('assets/background.png')
-             ),
-            ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 400,
-                width: double.infinity,
-                child: Column(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage('assets/background.png')
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+              width: double.infinity,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                      const Text('Sign in to continue'),
-                      Text(
-                        'Vegi',
-                        style: TextStyle(
+                    const Text('Sign in to continue'),
+                    Text(
+                      'Vegi',
+                      style: TextStyle(
                           fontSize: 50,
                           color: Colors.white,
                           shadows: [
                             BoxShadow(
                               blurRadius: 5,
                               color: Colors.green.shade900,
-                              offset: const Offset(3,3),
+                              offset: const Offset(3, 3),
                             )
-                          ]
-                        ),
-                      )
+                          ]),
+                    )
+                  ]),
+            ),
+            Column(
+              children: [
+                SignInButton(
+                  Buttons.Apple,
+                  text: "Sign in with Apple",
+                  onPressed: () {},
+                ),
+                SignInButton(
+                  Buttons.Google,
+                  text: "Sign in with Google",
+                  onPressed: () async {
+                  },
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'By signing in you are agreeing to our',
+                      style: TextStyle(
+                        color: Colors.grey[800],
+                      ),
+                    ),
+                    Text(
+                      'Terms and Pricacy Policy',
+                      style: TextStyle(
+                        color: Colors.grey[800],
+                      ),
+                    ),
                   ],
                 ),
+              ],
             ),
           ],
         ),
